788d9a87509f2f408eb950c4e63b6922c1f99d7a
chippieboi <ebcarter96@gmail.com>
2025-07-12 01:00:41 -0400

    part of part 2
    STUFF

--- part_1/src/Entities/Actors/Actions/action.gd
+++ part_1/src/Entities/Actors/Actions/action.gd
@@ -1,3 +1,2 @@
 class_name Action
 extends RefCounted
-
--- part_1/src/Utils/grid.gd
+++ part_1/src/Utils/grid.gd
@@ -3,12 +3,12 @@ extends Object
 
 const tile_size = Vector2i(16, 16)
 
-
+#convert vector2i grid position to vector2i world position
 static  func grid_to_world(grid_pos: Vector2i) -> Vector2i:
 	var world_pos: Vector2i = grid_pos * tile_size
 	return world_pos
 
-
+#convert vector2i world position to vector2i grid position
 static func world_to_grid(world_pos: Vector2i) -> Vector2i:
 	var grid_pos: Vector2i = world_pos / tile_size
 	return grid_pos
