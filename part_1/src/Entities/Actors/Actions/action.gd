271965ce3f0c6de90041701c40f8413ec66a83be
chippieboi <ebcarter96@gmail.com>
2025-08-08 02:06:49 -0400

    complete tutorial

--- part_1/src/Entities/Actors/Actions/action.gd
+++ part_1/src/Entities/Actors/Actions/action.gd
@@ -1,2 +1,13 @@
 class_name Action
 extends RefCounted
+
+var entity: Entity
+
+func _init(entity: Entity) -> void:
+	self.entity = entity
+
+func perform() -> bool:
+	return false
+
+func get_map_data() -> MapData:
+	return entity.map_data
--- part_1/src/Entities/Actors/Actions/escape_action.gd
+++ part_1/src/Entities/Actors/Actions/escape_action.gd
@@ -1,2 +1,7 @@
 class_name EscapeAction
 extends Action
+
+func perform() -> bool:
+	entity.map_data.save()
+	SignalBus.escape_requested.emit()
+	return false
